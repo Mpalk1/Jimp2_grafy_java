e99604ea4307f0977d197c8b4599c1f2b2e85e6a
Maciej Palkowski <maciej.palkowski.stud@pw.edu.pl>
2025-05-08 20:23:44 +0200

    repo init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        JFrame window = new JFrame("Jimp2 Grafy");
+        window.setResizable(false);
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setSize(1600, 1000);
+        window.setLocationRelativeTo(null);
+
+
+
+
+        window.setVisible(true); // to zawsze na koncu
+    }
 }
